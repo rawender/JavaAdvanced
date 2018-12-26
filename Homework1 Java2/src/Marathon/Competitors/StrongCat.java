ac65a7ae3b49cf5e8e32240b9f58384dc2870ab6
rawender <rawender@yandex.ru>
2018-12-26 23:05:14 +0300

    не со всем разобрался, но сдаю Домашнее задание

---
+++ Homework1 Java2/src/Marathon/Competitors/StrongCat.java
@@ -0,0 +1,7 @@
+package Marathon.Competitors;
+
+public class StrongCat extends Animal {
+    public StrongCat(String name) {
+        super("Кот", name, 300, 20, 5);
+    }
+}
---
+++ Homework1 Java2/src/Marathon/Obstacle/Water.java
@@ -0,0 +1,16 @@
+package Marathon.Obstacle;
+
+import Marathon.Competitors.Competitor;
+
+public class Water extends Obstacle {
+    int length;
+
+    public Water(int length) {
+        this.length = length;
+    }
+
+    @Override
+    public void doIt(Competitor competitor) {
+        competitor.swim(length);
+    }
+}
