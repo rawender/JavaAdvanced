5d49dbdc3a900854779ccf068190ca5fab51f70e
rawender <rawender@yandex.ru>
2019-01-16 15:25:11 +0300

    Revert "поправил домашнюю работу"
    This reverts commit 1fd0a58ba11fc0e2fb847e30d96bd84ea07c9dbf.

--- Homework5Java2/src/Main.java
+++ Homework5Java2/src/Main.java
@@ -3,28 +3,23 @@ public class Main {
     static final int size = 10000000;
     static final int h = size / 2;
 
-    static void fillArray(float[] arr) {
+    static float[] fillArray(float[] arr) {
         for (int i = 0; i < arr.length; i++) {
             arr[i] = 1;
         }
-
         long a = System.currentTimeMillis();
-
         for (int i = 0; i < arr.length; i++) {
             arr[i] = (float)(arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
         }
         System.out.println(System.currentTimeMillis() - a);
+        return arr;
     }
 
     static void disassembleAssemble(float[] arr) {
         float[] a1 = new float[h];
         float[] a2 = new float[h];
 
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = 1;
-        }
-
-        long a = System.currentTimeMillis();
+        long b = System.currentTimeMillis();
 
         Thread t1 = new Thread(new Runnable() {
             @Override
@@ -50,24 +45,19 @@ public class Main {
             e.printStackTrace();
         }
 
-        long b = System.currentTimeMillis();
-        System.out.println(b - a);
+        System.out.println(System.currentTimeMillis() - b);
 
         Thread t3 = new Thread(new Runnable() {
             @Override
             public void run() {
-                for (int i = 0; i < a1.length; i++) {
-                    a1[i] = (float)(a1[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
-                }
+                System.arraycopy(a1,0,arr,0,h);
             }
         });
 
         Thread t4 = new Thread(new Runnable() {
             @Override
             public void run() {
-                for (int i = 0; i < a2.length; i++) {
-                    a2[i] = (float)(a2[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
-                }
+                System.arraycopy(a2,0,arr,h,h);
             }
         });
 
@@ -81,35 +71,7 @@ public class Main {
             e.printStackTrace();
         }
 
-
-        long c = System.currentTimeMillis();
-        System.out.println(c - b);
-
-        Thread t5 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                System.arraycopy(a1,0,arr,0,h);
-            }
-        });
-
-        Thread t6 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                System.arraycopy(a2,0,arr,h,h);
-            }
-        });
-
-        t5.start();
-        t6.start();
-
-        try {
-            t5.join();
-            t6.join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        System.out.println(System.currentTimeMillis() - c);
+        System.out.println(System.currentTimeMillis() - b);
     }
 
     public static void main(String[] args) {
