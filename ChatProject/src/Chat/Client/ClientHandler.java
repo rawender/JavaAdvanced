944cbfd05285c8aaa676f8b187294994930ed34c
rawender <rawender@yandex.ru>
2019-01-22 01:48:03 +0300

    перенес основу с урока в отдельный проект

---
+++ ChatProject/src/Chat/Client/ClientHandler.java
@@ -0,0 +1,90 @@
+package Chat.Client;
+
+import Chat.Server.AuthService;
+import Chat.Server.ServerTest;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+public class ClientHandler {
+
+    private Socket socket;
+    private DataOutputStream out;
+    private DataInputStream in;
+    private ServerTest server;
+    private String nick;
+
+    public ClientHandler(ServerTest server, Socket socket) {
+        try {
+            this.socket = socket;
+            this.server = server;
+            this.in = new DataInputStream(socket.getInputStream());
+            this.out = new DataOutputStream(socket.getOutputStream());
+
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        while (true) {
+                            String str = in.readUTF();
+
+                            if (str.startsWith("/auth")) {
+                                String[] tokens = str.split(" ");
+                                String newNick = AuthService.getNickLoginAndPass(tokens[1], tokens[2]);
+                                if (newNick != null) {
+                                    sendMsg("/authok");
+                                    nick = newNick;
+                                    server.subscribe(ClientHandler.this);
+                                    break;
+                                } else {
+                                    sendMsg("Неверный логин/пароль!");
+                                }
+                            }
+                        }
+
+                        while (true) {
+                            String str = in.readUTF();
+                            if(str.equals("/end")) {
+                                out.writeUTF("/serverClosed");
+                                break;
+                            }
+                            server.broadcastMsg(str);
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    } finally {
+                        try {
+                            in.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        try {
+                            out.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        try {
+                            socket.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        server.unsubscribe(ClientHandler.this);
+                    }
+
+                }
+            }).start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void sendMsg(String msg) {
+        try {
+            out.writeUTF(msg);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ ChatProject/src/Chat/Server/ServerTest.java
@@ -0,0 +1,58 @@
+package Chat.Server;
+
+import Chat.Client.ClientHandler;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Vector;
+
+public class ServerTest {
+    private Vector<ClientHandler> clients;
+
+    public ServerTest(){
+        clients = new Vector<>();
+        ServerSocket server = null;
+        Socket socket = null;
+
+        try {
+            AuthService.connect();
+            server = new ServerSocket(8189);
+            System.out.println("Сервер запущен!");
+
+            while (true) {
+                socket = server.accept();
+                System.out.println("Клиент подключился");
+                new ClientHandler(this,socket);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                server.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            AuthService.disconnect();
+        }
+    }
+
+    public void subscribe(ClientHandler client) {
+        clients.add(client);
+    }
+
+    public void unsubscribe(ClientHandler client) {
+        clients.remove(client);
+    }
+
+    public void broadcastMsg(String msg) {
+        for (ClientHandler o: clients) {
+            o.sendMsg(msg);
+        }
+    }
+}
